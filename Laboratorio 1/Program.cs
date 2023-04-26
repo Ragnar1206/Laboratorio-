896006f24f149edd62dcf4f989d206e13443f726
Julian <Julian@LAPTOP-3J9FFDDU>
2023-04-26 16:57:07 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su nombre");
+            string Nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine(" Soy " + Nombre);
+
+            /* COMENTARIOS */
+
+            Console.Write("Hola mundo");
+            Console.Write(" Soy " + Nombre);
+            Console.ReadKey();
+
+        }
+    }
+}
